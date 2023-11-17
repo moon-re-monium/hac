1c7f785d100da81d797e63ff716acf89cb35b869
moon-re-monium <137092187+moon-re-monium@users.noreply.github.com>
2023-11-17 22:38:34 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -1,31 +1,41 @@
-const Quagga = require('quagga');
-
-
 document.addEventListener('DOMContentLoaded', function () {
     const interactive = document.getElementById('interactive');
 
-    Quagga.init({
-        inputStream: {
-            name: "Live",
-            type: "LiveStream",
-            target: interactive
-        },
-        decoder: {
-            readers: ["code_128_reader", "ean_reader", "ean_8_reader", "code_39_reader", "code_39_vin_reader", "codabar_reader", "upc_reader", "upc_e_reader", "i2of5_reader", "2of5_reader", "code_93_reader"]
-        }
-    }, function (err) {
-        if (err) {
-            console.error(err);
-            return;
-        }
-        Quagga.start();
-    });
+    // Check for camera access permission
+    navigator.mediaDevices.getUserMedia({ video: true })
+        .then(function (stream) {
+            // If permission is granted, proceed with Quagga initialization
+            Quagga.init({
+                inputStream: {
+                    name: "Live",
+                    type: "LiveStream",
+                    target: interactive,
+                    constraints: {
+                        width: 640,
+                        height: 480,
+                        facingMode: "environment" // or "user" for front camera
+                    }
+                },
+                decoder: {
+                    readers: ["code_128_reader", "ean_reader", "ean_8_reader", "code_39_reader", "code_39_vin_reader", "codabar_reader", "upc_reader", "upc_e_reader", "i2of5_reader", "2of5_reader", "code_93_reader"]
+                }
+            }, function (err) {
+                if (err) {
+                    console.error(err);
+                    return;
+                }
+                Quagga.start();
+            });
 
-    Quagga.onDetected(function (result) {
-        const code = result.codeResult.code;
-        displayFilePreview(code);
-        Quagga.stop();
-    });
+            Quagga.onDetected(function (result) {
+                const code = result.codeResult.code;
+                displayFilePreview(code);
+                Quagga.stop();
+            });
+        })
+        .catch(function (err) {
+            console.error("Error accessing camera: ", err);
+        });
 
     function displayFilePreview(data) {
         // Assuming the data is a base64-encoded string
